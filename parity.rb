fb2e98cc9fd49c1f8070387bfd6c0dfd2d2d51a8
André Silva <andre.beat@gmail.com>
2018-03-07 13:06:07 +0000

    use homebrew support for setting development and head versions

--- parity.rb
+++ parity.rb
@@ -2,23 +2,20 @@ require 'formula'
 
 class Parity < Formula
   homepage 'https://github.com/paritytech/parity'
+  version '1.8.11'
+  url 'https://d1h4xl4cr1h0mo.cloudfront.net/v1.8.11/x86_64-apple-darwin/parity'
+  sha256 "e52e16b11f25cc8900349df04b0ce9b22bb85372029e67608153ae5ea9e02af4"
 
-  if build.include? "master" or build.include? "nightly"
-    version '1.10.0'
-    url 'https://d1h4xl4cr1h0mo.cloudfront.net/nightly/x86_64-apple-darwin/parity'
-  elsif build.include? "beta" or build.include? "latest"
+  devel do
     version '1.9.4'
     url 'https://d1h4xl4cr1h0mo.cloudfront.net/v1.9.4/x86_64-apple-darwin/parity'
     sha256 "95ee40205278776f03918178e554e67c36177bfa53bb09a5c22fe5ca08dc1c36"
-  else
-    version '1.8.11'
-    url 'https://d1h4xl4cr1h0mo.cloudfront.net/v1.8.11/x86_64-apple-darwin/parity'
-    sha256 "e52e16b11f25cc8900349df04b0ce9b22bb85372029e67608153ae5ea9e02af4"
   end
 
-  option 'nightly', 'Install nightly version.'
-  option 'beta', 'Install latest beta.'
-  option 'stable', 'Install latest stable (default).'
+  head do
+    version '1.10.0'
+    url 'https://d1h4xl4cr1h0mo.cloudfront.net/nightly/x86_64-apple-darwin/parity'
+  end
 
   bottle :unneeded
 
